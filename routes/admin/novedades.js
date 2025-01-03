3fa94775d44b22255ae3b92ebd3863b352462781
FAguirre07 <lionelfaguirre@gmail.com>
2025-01-02 21:31:11 -0300

    M3U5

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,76 +1,129 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload)
+const destroy = util.promisify(cloudinary.uploader.destroy)
 
-var novedadesModel = require ('../../models/novedadesModel');
+router.get('/', async function (req, res, next) {
 
-router.get('/', async function(req,res,next){
-    
     var novedades = await novedadesModel.getNovedades();
+
+    novedades = novedades.map(novedad => {
+        if (novedad.img_id) {
+            const imagen = cloudinary.image(novedad.img_id, {
+                width: 100,
+                height: 100,
+                crop: 'fill'
+            });
+            return {
+                ...novedad,
+                imagen
+            }
+        } else {
+            return {
+                ...novedad,
+                imagen: ''
+            }
+        }
+    });
+
     res.render('admin/novedades', {
-        layout:'admin/layout',
+        layout: 'admin/layout',
         usuario: req.session.nombre,
         novedades
     });
 });
 
-router.get('/eliminar/:id_emp', async function(req,res,next){
+router.get('/eliminar/:id_emp', async function (req, res, next) {
     var id = req.params.id;
+    
+    let novedad = await novedadesModel.deleteNovedadesById(id_emp);
+    if (novedad.img_id) {
+        await destroy(novedad.img_id);
+    }
     await novedadesModel.deleteNovedadesById(id_emp);
     res.redirect('/admin/novedades');
 });
 
-router.get('/agregar', function(req,res,next) {
-    res.render('admin/agregar' , {
-        layout: 'admin/layout' 
-});
+
+
+router.get('/agregar', function (req, res, next) {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    });
 });
 
-router.post('/agregar', async function(req,res,next){
+router.post('/agregar', async function (req, res, next) {
     try {
+        var img_id = '';
+        if (req.files && Object.keys(req.files).length > 0) {
+            imagen = req.files.imagen;
+            img_id = (await uploader(imagen.tempFilePath)).public_id;
+        }
+
         if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
-            await novedadesModel.insertNovedades(req.body);
+            await novedadesModel.insertNovedades({ ...req.body, img_id });
             res.redirect('/admin/novedades');
         } else {
-            res.render('admin/agregar' , {
+            res.render('admin/agregar', {
                 layout: 'admin/layout',
                 error: true, message: "Todos los campos son obligatorios"
             })
         }
-        } catch (error) {
-            console.log(error);
-            res.render('admin/agregar' , {
-                layout: 'admin/layout',
-                error: true, message: 'No se cargo la novedad correctamente'
-            });
-        }
-    });
-
-    router.get('/modificar/:id', async function(req,res,next){
-        var id_emp = req.params.id_emp;
-        var novedad = await novedadesModel.getNovedadesById(id_emp);
-        res.render('admin/modificar' , {
+    } catch (error) {
+        console.log(error);
+        res.render('admin/agregar', {
             layout: 'admin/layout',
-            usuario: req.session.nombre,
-            novedad
+            error: true, message: 'No se cargo la novedad correctamente'
         });
-    });
+    }
+});
 
-    router.post('/modificar', async function(req,res,next){
-        try {
-            let obj = {
-                titulo: req.body.titulo,
-                subtitulo: req.body.subtitulo,
-                cuerpo: req.body.cuerpo
+router.get('/modificar/:id', async function (req, res, next) {
+    var id_emp = req.params.id_emp;
+    var novedad = await novedadesModel.getNovedadesById(id_emp);
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        usuario: req.session.nombre,
+        novedad
+    });
+});
+
+router.post('/modificar', async function (req, res, next) {
+    try {
+
+        let img_id = req.body.img_original;
+        let borrar_img_vieja = false;
+        if (req.body.img_delete === "1") {
+            img_id = null;
+            borrar_img_vieja = true;
+        } else {
+            if (req.files && Object.keys(req.files).length > 0) {
+                imagen = req.files.imagen;
+                img_id = (await uploader(imagen.tempFilePath)).public_id;
+                borrar_img_vieja = true;
             }
-            await novedadesModel.modificarNovedadesById(req.body.id_emp, obj);
-            res.redirect('/admin/novedades');
-        } catch (error) {
-            console.log(error);
-            res.render('admin/modificar' , {
-                layout: 'admin/layout',
-                error: true, message: 'No se cargo la novedad correctamente'
-            });
         }
-    }); 
+        if (borrar_img_vieja && req.body.img_original) {
+            await destroy(req.body.img_original);
+        }
+        var obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo,
+            img_id
+        }
+        await novedadesModel.modificarNovedadesById(req.body.id_emp, obj);
+        res.redirect('/admin/novedades');
+    } catch (error) {
+        console.log(error);
+        res.render('admin/modificar', {
+            layout: 'admin/layout',
+            error: true, message: 'No se cargo la novedad correctamente'
+        });
+    }
+});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,50 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
+
 
 router.get("/", async function (req, res, next) {
-
-  var novedades = await novedadesModel.getNovedades();
+  novedades = await novedadesModel.getNovedades();
+  novedades = novedades.splice(0, 5);
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: '/img/doñamaltalogo.png'
+      }
+    }
+  });
   res.render("index", {
     novedades
-  });
-  res.render("layout");
+});
 });
 
 /* GET home page. */
-router.post('/contacto', async function (req, res, next)  {
+router.post('/contacto', async function (req, res, next) {
   var nombre = req.body.nombre;
   var apellido = req.body.apellido;
   var email = req.body.email;
   var telefono = req.body.telefono;
   var ciudad = req.body.ciudad
-  
+
   console.log(req.body)
-  
+
   var obj = {
     to: 'fabriciolaguirre@gmail.com',
     subject: 'Contacto desde la web',
-    html:` ${nombre} ${apellido} Se contacto a traves de la página y quiere mas informacion del producto`
+    html: ` ${nombre} ${apellido} Se contacto a traves de la página y quiere mas informacion del producto`
   }
-  
+
   var transport = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
@@ -36,9 +54,9 @@ router.post('/contacto', async function (req, res, next)  {
       pass: process.env.SMTP_PASS,
     }
   });
-  
+
   var info = await transport.sendMail(obj);
-  
+
   res.render('index', {
     message: 'Mensaje enviado satisfactoriamente',
   });
